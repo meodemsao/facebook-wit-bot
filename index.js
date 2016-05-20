b28734ad9cc7724ea051f84a6a3c895d80824a43
Vũ Công Bằng <bangvc@vgroup.vn>
2016-05-20 18:33:53 +0700

    update

---
+++ index.js
@@ -0,0 +1,47 @@
+var Botkit = require('botkit');
+var Witbot = require('witbot');
+
+var facebookToken = process.env.FACEBOOK_TOKEN;
+var verifyToken = process.env.VERIFY_TOKEN;
+var witbot = Witbot(process.env.WIT_TOKEN);
+var controller = Botkit.facebookbot({ 
+  debug: false,
+  access_token: facebookToken,
+  verify_token: verifyToken
+});
+
+controller.spawn({}).startRTM(function (err, bot, payload) {
+  if (err) throw new Error('Error connecting to Facebook: ', err);
+  console.log('Connected to Facebook');
+})
+
+// wire up DMs and direct mentions to wit.ai
+controller.hears('.*', 'direct_message,direct_mention', function (bot, message) {
+  var wit = witbot.process(message.text, bot, message)
+
+  wit.hears('hello', 0.53, function (bot, message, outcome) {
+    bot.startConversation(message, function (_, convo) {
+      convo.say('Hello!')
+      convo.ask('How are you?', function (response, convo) {
+        witbot.process(response.text)
+          .hears('good', 0.5, function (outcome) {
+            convo.say('I am so glad to hear it!')
+            convo.next()
+          })
+          .hears('bad', 0.5, function (outcome) {
+            convo.say('I\'m sorry, that is terrible')
+            convo.next()
+          })
+          .otherwise(function (outcome) {
+            convo.say('I\'m cofused')
+            convo.repeat()
+            convo.next()
+          })
+      })
+    })
+  })
+
+  wit.otherwise(function (bot, message) {
+    bot.reply(message, 'You are so intelligent, and I am so simple. I don\'t understnd')
+  })
+})
